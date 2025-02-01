9748c11e7c43b6c1bdf9992cf356dc9db77d3413
devingryu <devingryu@korea.ac.kr>
2025-02-02 02:05:59 +0900

    ftr: i18n, uiresources

---
+++ src/lib/i18n/i18n.ts
@@ -0,0 +1,15 @@
+import { browser } from '$app/environment';
+import {
+  init,
+  register,
+} from 'svelte-i18n';
+
+const defaultLocale = 'en';
+
+register('en', () => import('./en.json'));
+register('ko', () => import('./ko.json'));
+
+init({
+  fallbackLocale: defaultLocale,
+  initialLocale: browser ? window.navigator.language : defaultLocale,
+});
--- src/routes/(sidebar)/item/+page.svelte
+++ src/routes/(sidebar)/item/+page.svelte
@@ -1,1 +1,23 @@
-<h1 style="text-4xl">여기에는 아무것도 없습니다.</h1>
+<script lang="ts">
+  import * as Breadcrumb from '$lib/components/ui/breadcrumb/index.js';
+  import BodyWrapper from '$lib/custom/BodyWrapper.svelte';
+  import { _ } from 'svelte-i18n';
+</script>
+
+<BodyWrapper>
+  <header>
+    <Breadcrumb.Root class="mb-4">
+      <Breadcrumb.List>
+        <Breadcrumb.Item>
+          <Breadcrumb.Link href="/">{$_('page.home')}</Breadcrumb.Link>
+        </Breadcrumb.Item>
+        <Breadcrumb.Separator />
+        <Breadcrumb.Item>
+          <Breadcrumb.Page>{$_('page.items')}</Breadcrumb.Page>
+        </Breadcrumb.Item>
+      </Breadcrumb.List>
+    </Breadcrumb.Root>
+
+    <h1 class="text-4xl font-bold">{$_('page.items')}</h1>
+  </header>
+</BodyWrapper>
