92bf4f88a4233713cf60632b40094393740a6409
devingryu <devingryu@korea.ac.kr>
2025-02-03 01:48:33 +0900

    ftr: nprogress

--- src/routes/(sidebar)/+layout.svelte
+++ src/routes/(sidebar)/+layout.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
   import '../../app.css';
-  import * as Sidebar from '$lib/components/ui/sidebar';
+  import { beforeNavigate, afterNavigate } from '$app/navigation';
+  import NProgress from 'nprogress';
+  import 'nprogress/nprogress.css';
   // import AppSidebar from '$lib/components/app-sidebar.svelte';
   // import { ModeWatcher } from 'mode-watcher';
   import { Toaster } from 'svelte-5-french-toast';
   import ActionBar from '$lib/custom/ActionBar.svelte';
   let { children } = $props();
+
+  // 라우팅 시작 시 실행
+  beforeNavigate(({ to }) => {
+    if (to) NProgress.start();
+  });
+
+  // 라우팅 완료 시 실행
+  afterNavigate(() => {
+    NProgress.done();
+  });
 </script>
 
 <Toaster />
